055f5b8e03df1863d2a69093ac0c42a14fc8e922
keplerdani <danielkepler@pm.me>
2023-08-04 08:49:05 +0200

    solve warmup exercise

--- 014-objects/exercises/warmup-exercise.js
+++ 014-objects/exercises/warmup-exercise.js
@@ -13,3 +13,17 @@
     
     5. Print out "mexico" to the terminal again
 */
+const mexico = {
+  id: 24,
+  name: "Mexico",
+  capital: "Mexico City",
+  neighbours: ["USA", "Guatemala", "Belize"],
+};
+
+console.log(mexico.name);
+
+mexico.id = 25;
+
+mexico.neighbours.push(("Honduras"))
+
+console.log(mexico.name);
