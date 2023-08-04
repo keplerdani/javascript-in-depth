c6971fa8b890019d0bd487f16ed0929223dc16c7
keplerdani <danielkepler@pm.me>
2023-08-04 10:38:06 +0200

    solve exercise 3

--- 014-objects/exercises/exercise-3.js
+++ 014-objects/exercises/exercise-3.js
@@ -21,3 +21,32 @@
 
     *This is a challenging exercise - take it slow and step by step
 */
+const houseForSale = {
+  area: 940,
+  value: 320000,
+  streetName: "Fifth Street",
+  built: "2012",
+  owner: { name: "Blake", age: 29 },
+  offers: [290000, 295000, 315000, 312000],
+};
+
+console.log(houseForSale);
+
+delete houseForSale.built;
+
+houseForSale.owner.age = 30;
+
+// console.log(Math.max(...houseForSale.offers));
+
+console.log(
+  houseForSale.offers.reduce((max, price) => {
+    if (max < price) {
+      return price;
+    }
+    return max;
+  })
+);
+
+houseForSale["sale price"] = 312000;
+
+// console.log(houseForSale);
