4885fc90f71098fa30e04dabbb231ee2e1b796ab
keplerdani <danielkepler@pm.me>
2023-08-04 08:58:05 +0200

    solve exercise 1

--- 014-objects/exercises/exercise-1.js
+++ 014-objects/exercises/exercise-1.js
@@ -20,3 +20,32 @@
 
     8. Print out "myPet" to the terminal again
 */
+// 1.
+const myPet = {
+  name: "Sudo",
+  type: "Dog",
+  breed: "Poodle",
+  age: 7,
+  friends: ["Bit", "Byte", "Data"],
+};
+
+// 2. Print out "myPet" to the terminal
+console.log(myPet);
+
+// 3. Add a new property: colour -> "Black"
+myPet.colour = "Black";
+
+// 4. Change "breed" to be "Beagle"
+myPet.breed = "Beagle";
+
+// 5. Remove "Data" from the list of friends for "myPet"
+myPet.friends.pop();
+
+// 6. Print out "myPet" to the terminal again
+console.log(myPet);
+
+// 7. Add "Chip" to the list of friends for "myPet"
+myPet.friends.push("Chip");
+
+// 8. Print out "myPet" to the terminal again
+console.log(myPet);
