0d272bbe953b9656cb0e3093a4aef2f16d0fdb97
keplerdani <danielkepler@pm.me>
2023-08-05 10:10:54 +0200

    improve solution

--- 007-while-loops/exercises/problem-set-warmup.js
+++ 007-while-loops/exercises/problem-set-warmup.js
@@ -2,10 +2,7 @@
     Create a while loop that prints out the numbers from 5 - 10 (inclusive)
 */
 let i = 5;
-while (true) {
+while (i <= 10) {
   console.log(i);
-  if (i === 10) {
-    break;
-  }
   i++;
 }
