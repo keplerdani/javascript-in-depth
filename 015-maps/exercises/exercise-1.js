7a70a63a32bad13d6140e84b9698fa66cc20207e
keplerdani <danielkepler@pm.me>
2023-08-06 18:28:23 +0200

    solve exercise 1

--- 015-maps/exercises/exercise-1.js
+++ 015-maps/exercises/exercise-1.js
@@ -20,3 +20,23 @@
 
     8. Print out "myPet" to the terminal again
 */
+
+const myPet = new Map();
+myPet.set("name", "Sudo");
+myPet.set("type", "Dog");
+myPet.set("breed", "Poodle");
+myPet.set("age", 7);
+myPet.set("friends", ["Bit", "Byte", "Data"]);
+
+console.log(myPet);
+
+myPet.set("colour", "Black");
+myPet.set("breed", "Beagle");
+
+myPet.get("friends").pop();
+
+console.log(myPet);
+
+myPet.get("friends").push("Chip");
+
+console.log(myPet);
