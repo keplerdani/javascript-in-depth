61d956457c584027a12cd2c02c2558279596b70a
keplerdani <danielkepler@pm.me>
2023-08-06 18:09:44 +0200

    solve warmup

--- 015-maps/exercises/warmup-exercise.js
+++ 015-maps/exercises/warmup-exercise.js
@@ -13,3 +13,17 @@
     
     5. Print out "mexico" to the terminal again
 */
+
+const mexico = new Map();
+
+mexico.set("id", 24);
+mexico.set("name", "Mexico");
+mexico.set("capital", "Mexico City");
+mexico.set("neighbours", ["USA", "Guatemala", "Belize"]);
+
+console.log(mexico);
+
+mexico.set("id", 25);
+
+mexico.get("neighbours").push("Honduras")
+console.log(mexico);
